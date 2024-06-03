975f8b668c203af46fa16887afc4785e9d0b7912
NguyenVH01 <nguyenvuhoangwork@gmail.com>
2024-06-04 01:36:38 +0700

    feature/demo - Add subtract/multiply function

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ def sum(a, b):
     return a + b
 
 
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
 if __name__ == "__main__":
     print(f"Total : {sum(1, 2)}")
