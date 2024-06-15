049abb816db0cee62a5bba005e58912ce7ec10aa
NguyenVH01 <nguyenvuhoangwork@gmail.com>
2024-06-15 17:09:27 +0700

    add function to check divide by zero

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Calculate sum a, b
 def sum(a, b):
-    return a + b
+  return a + b
+
+def check_divide_by_zero(number):
+  if (number == 0):
+    return False
+  else:
+    return True
 
 
 if __name__ == "__main__":
-    print(f"Total : {sum(1, 2)}")
+  print(f"Total : {sum(1, 2)}")
